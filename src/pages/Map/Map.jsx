da257706281fd0aad1a88752a4ef2402273f4bb6
kyracho <wsc2119@columbia.edu>
2024-09-03 23:18:29 -0700

    add navbar with home, map, about, and github

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -1,8 +1,10 @@
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
 import MapComponent from '../../components/MapComponent/MapComponent'
 import SearchComponent from '../../components/SearchbarComponent/SearchComponent';
 import useFetch from '../../hooks/usefetch';
 import styles from './Map.module.css'
 function Map () {
+    const navigate = useNavigate();  // Initialize navigate
     const { data, isLoading, error } = useFetch('/api/linkdokument/OpenData/Laddstation_fordon.json');
     if (isLoading) {
         return <div>Loading...</div>;
@@ -14,6 +16,20 @@ function Map () {
         <div className={styles.base}>
             <SearchComponent />
             <MapComponent fetchData={data}/>
+            <div className={styles.buttons}>
+                <button className={styles.button} onClick={() => navigate("/")}>
+                    Home
+                </button>
+                <button className={styles.button} onClick={() => navigate("/map")}>
+                    Map
+                </button>
+                <button className={styles.button} onClick={() => navigate("/about")}>
+                    About
+                </button>
+                <a href="https://github.com/Fridish/Charging-Stations" className={styles.button} target="_blank" rel="noopener noreferrer">
+                    GitHub
+                </a>
+            </div>
         </div>
     )
 }
